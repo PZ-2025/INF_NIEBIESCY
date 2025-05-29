b1f26fb78b4f26a523f6d1599db384120b341006
Karolina Madej <you@example.com>
2025-05-29 12:43:46 +0200

    dodanie dostepnych ksiazek dla czytelnika

--- demo/src/main/java/com/example/demo/AvailableBooksController.java
+++ demo/src/main/java/com/example/demo/AvailableBooksController.java
@@ -1,11 +1,20 @@
 package com.example.demo;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import java.io.IOException;
 
@@ -18,6 +27,25 @@ public class AvailableBooksController {
     private Label reservationButton;
     @FXML
     private Label logoutButton;
+    @FXML
+    private TableView<Book> tableView;
+
+    @FXML
+    private TableColumn<Book, String> autorColumn;
+
+    @FXML
+    private TableColumn<Book, String> tytulColumn;
+
+    @FXML
+    private TableColumn<Book, String> gatunekColumn;
+
+    @FXML
+    private TableColumn<Book, String> wydawnictwoColumn;
+
+    @FXML
+    private TableColumn<Book, String> iloscColumn;
+
+    private ObservableList<Book> bookList = FXCollections.observableArrayList();
 
     private Czytelnik aktualnyCzytelnik;
     public void setAktualnyCzytelnik(Czytelnik czytelnik) {
@@ -29,6 +57,45 @@ public class AvailableBooksController {
         booksButton.setOnMouseClicked(this::otworzKsiegozbior);
         reservationButton.setOnMouseClicked(this::otworzRezerwacje);
         logoutButton.setOnMouseClicked(this::wyloguj);
+
+        autorColumn.setCellValueFactory(new PropertyValueFactory<>("autor"));
+        tytulColumn.setCellValueFactory(new PropertyValueFactory<>("tytul"));
+        gatunekColumn.setCellValueFactory(new PropertyValueFactory<>("gatunek"));
+        wydawnictwoColumn.setCellValueFactory(new PropertyValueFactory<>("wydawnictwo"));
+        iloscColumn.setCellValueFactory(new PropertyValueFactory<>("ilosc"));
+
+        loadBooks();
+    }
+
+    private void loadBooks() {
+        String query = "SELECT k.tytul, a.nazwa AS autor, g.nazwa_gatunku AS gatunek, " +
+                "k.wydawnictwo, k.ilosc " +
+                "FROM ksiazki k " +
+                "JOIN autorzy a ON k.id_autora = a.id_autora " +
+                "JOIN gatunek g ON k.id_gatunku = g.id_gatunku";
+
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        try (PreparedStatement pstmt = connectDB.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                Book book = new Book(
+                        rs.getString("autor"),
+                        rs.getString("tytul"),
+                        rs.getString("gatunek"),
+                        rs.getString("wydawnictwo"),
+                        Integer.toString(rs.getInt("ilosc"))
+                );
+                bookList.add(book);
+            }
+
+            tableView.setItems(bookList);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void otworzHistorie(javafx.scene.input.MouseEvent mouseEvent) {
@@ -102,9 +169,6 @@ public class AvailableBooksController {
                 return;
             }
             BorderPane root = fxmlLoader.load();
-            AvailableBooksController controller = fxmlLoader.getController();
-            controller.setAktualnyCzytelnik(aktualnyCzytelnik);
-
             Stage stage = (Stage) logoutButton.getScene().getWindow();
             Scene scene = new Scene(root, 600, 400);
             stage.setScene(scene);
